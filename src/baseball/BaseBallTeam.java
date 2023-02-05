5057e7e37982e58ad369d798e51e79b942f2a665
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-02-05 19:15:06 +0900

    修正　Lesson５課題

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -2,21 +2,51 @@ package baseball;
 
 public class BaseBallTeam {
     // フィールド
-    public String name; // チーム名
-    public int win; // 勝
-    public int lose; // 負
-    public int draw; // 同点
+    private String name; // チーム名
+    private int win; // 勝
+    private int lose; // 負
+    private int draw; // 同点
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
 
     // 勝率メソッド
-    public double getRate(int win, int lose) {
-        double getRate = ((double) win / (win + lose));
-        return getRate;
+    public double getRate() {
+        return (double) this.win / (this.win + this.lose);
     }
 
     // reportメソッド
-    public void report(String name, int win, int lose, int draw) {
-        System.out.println(
-                name + "の2022年の成績は " + win + "勝 " + lose + "敗 " + draw + "分、勝率は " + getRate(win, lose) + "です。");
-
+    public void report() {
+        System.out.println(this.name + "の2022年の成績は " + this.win + "勝 " + this.lose + "敗 " + this.draw + "分、勝率は "
+                + this.getRate() + "です。");
     }
 }
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -5,26 +5,50 @@ public class Review03 {
     public static void main(String[] args) {
 
         BaseBallTeam base1 = new BaseBallTeam();
-        base1.report("東京ヤクルトスワローズ", 80, 59, 4);
+        base1.setName("東京ヤクルトスワローズ");
+        base1.setWin(80);
+        base1.setLose(59);
+        base1.setDraw(4);
+        base1.report();
 
 
         BaseBallTeam base2 = new BaseBallTeam();
-        base2.report("横浜DeNAベイスターズ", 73, 68, 2);
+        base2.setName("横浜DeNAベイスターズ");
+        base2.setWin(73);
+        base2.setLose(68);
+        base2.setDraw(2);
+        base2.report();
 
 
         BaseBallTeam base3 = new BaseBallTeam();
-        base3.report("阪神タイガース", 68, 71, 4);
+        base3.setName("阪神タイガース");
+        base3.setWin(68);
+        base3.setLose(71);
+        base3.setDraw(4);
+        base3.report();
 
 
         BaseBallTeam base4 = new BaseBallTeam();
-        base4.report("読売ジャイアンツ", 68, 72, 3);
+        base4.setName("読売ジャイアンツ");
+        base4.setWin(68);
+        base4.setLose(72);
+        base4.setDraw(3);
+        base4.report();
 
 
         BaseBallTeam base5 = new BaseBallTeam();
-        base5.report("広島東洋カープ", 66, 74, 3);
+        base5.setName("広島東洋カープ");
+        base5.setWin(66);
+        base5.setLose(74);
+        base5.setDraw(3);
+        base5.report();
 
 
         BaseBallTeam base6 = new BaseBallTeam();
-        base6.report("中日ドラゴンズ", 66, 75, 2);
+        base6.setName("中日ドラゴンズ");
+        base6.setWin(66);
+        base6.setLose(75);
+        base6.setDraw(2);
+        base6.report();
     }
 }
